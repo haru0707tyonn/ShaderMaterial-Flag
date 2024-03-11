111ee668885a73cf1c6045f9442c885aaeb164b1
haru0707tyonn <145511049+haru0707tyonn@users.noreply.github.com>
2024-03-11 10:48:59 +0900

    Add files via upload

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -1,6 +1,12 @@
-// fragmetShaderは色
-precision mediump float; // RawShaderMaterialを使う場合に記述する必要がある　どれだけ詳細に決めるのか　ほかにはhighupなどがある 
+uniform vec3 uColor; // r, g, b
+uniform sampler2D uTexture; // 23 textureをつけるときのお決まりの型
+
+varying vec2 vUv; // 24 VertexShaderからfragmentShaderに変数を渡すときに使う
+varying float vElevation; // 25　影をつける
 
 void main() {
-    gl_FragColor = vec4(0.0,0.5, 1.0, 1.0); // R G B a（透明度）　透明度を設定するにはindex.jsのほうにtransparent: trueを記述する必要がある　 1を超える値はいれない
+    vec4 textureColor = texture2D(uTexture, vUv); // 24
+    // gl_FragColor = vec4(uColor, 1.0); // R G B a（透明度）　透明度を設定するにはindex.jsのほうにtransparent: trueを記述する必要がある　 1を超える値はいれない
+    textureColor.rgb *= vElevation * 2.5 + 0.7; // 25 影をつける
+    gl_FragColor = textureColor;
 }
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,22 +1,25 @@
-// vertexShaderは頂点
-// 9 uniformはグローバル変数を入れる　 4x4の行列 一文字も間違えずに（エラーになる）
-uniform mat4 projectionMatrix; // カメラがどこからどこまでを映すかの情報
-uniform mat4 modelMatrix; // 物体の位置の情報（行列）　回転、大きさなど
-uniform mat4 viewMatrix; // カメラの位置（行列）
+uniform vec2 uFrequency; // index.jsから値をインポート
+uniform float uTime; // 21
 
-attribute vec3 position; // x,y,zの座標 attributeは頂点情報などを入れる
-
-float a = 1.0; // 整数値だけ（1）の記述だとエラー
-float b = 2.0;
-float c = a + b; // 同じ型同士の計算は可能
+varying vec2 vUv; // 24 VertexShaderからfragmentShaderに変数を渡すときに使う
+varying float vElevation; // 25 影をつける
 
 void main() { // 書き方はC言語に似ている 自動的に実行されるので main(); を書く必要はない
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.x += 0.3; // 14 物体の位置を変更
+    // modelPosition.x += 0.3; // 14 物体の位置を変更
+    float elevation = sin(modelPosition.x * uFrequency.x + uTime) * 0.1;  // 17 21 25 modelPosition.xは物体の端から端 　x0.1をすることで振幅が減る x軸の波
+    elevation += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
+    
+    modelPosition.z += elevation;
+
+    // modelPosition.y *= 0.6; // 26 大きさが変わる 今回がindex.jsにて行う
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
     gl_Position = projectionPosition;
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+    vUv = uv; // 24
+    vElevation = elevation; // 25
 }
